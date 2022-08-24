bdd1201fccf39794c696df6260f6a4b6b9bb90e8
Phil Rajewski <philipandrew@gmail.com>
2022-08-23 19:31:47 -0700

    Adding Google OAuth20 and Routes

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI: 'mongodb+srv://phil-storybooks:J3va104rvLbPm3ED@storybooks-dev.i4kqulq.mongodb.net/?retryWrites=true&w=majority',
+  googleClientID: '966416843997-hjperso5p2kiu8k2c2g70gq9revk16mr.apps.googleusercontent.com',
+  googleClientSecret: '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'
+}
