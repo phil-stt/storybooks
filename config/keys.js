cee0fe0e4d769ab872ff380cc90d0715854e4e24
Phil Rajewski <philipandrew@gmail.com>
2022-09-07 10:54:50 -0700

    removing env variables/keys in code base

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,6 @@
 module.exports = {
-  mongoURI: 'mongodb+srv://phil-storybooks:J3va104rvLbPm3ED@storybooks-dev.i4kqulq.mongodb.net/?retryWrites=true&w=majority',
-  googleClientID: '966416843997-hjperso5p2kiu8k2c2g70gq9revk16mr.apps.googleusercontent.com',
-  googleClientSecret: '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'
+  mongoURI: process.env.DATABASE_URL,
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET
 }
+ 
